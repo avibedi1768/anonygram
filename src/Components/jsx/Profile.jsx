e95ef299c212cd931b15bfc5c83276252e936629
Arshpreet Singh Bedi <arshbedi2517@gmail.com>
2023-09-16 23:02:25 +0530

    changes

--- src/Components/jsx/Profile.jsx
+++ src/Components/jsx/Profile.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react'
-import LoadMsg from './LoadMsg'
+import React, { useState, useEffect } from 'react'
+// import LoadMsg from './LoadMsg'
 
 //import firebase
 import { db } from '../../firebase'
 import { get, ref } from 'firebase/database'
 
 function Profile() {
-  let [msgs, setMsgs] = useState([])
+  let [msgs, setMsgs] = useState([]);
   let user = window.location.href.split('#')[1];
 
   //first we will do for an example: raghav
   var n;
-  function show() {
+  useEffect(() => {
     get(ref(db, 'username/' + user + '/credentials/number'))
       .then((snapshot) => {
         n = snapshot.val()
@@ -20,7 +20,7 @@ function Profile() {
         //do loop from 1 to n, and do 'get' for each message
         loop(n)
       })
-  }
+  }, []);
 
   //for msgs: 0 to n
   function loop(n) {
@@ -32,7 +32,7 @@ function Profile() {
         });
       promises.push(promise);
     }
-  
+
     Promise.all(promises).then((messages) => {
       console.log(messages);
       setMsgs(messages);
@@ -41,13 +41,20 @@ function Profile() {
 
   return (
     <div>
-      {<LoadMsg msg={user} />}
-      <button onClick={show}>check</button><br />
-      <ul>
-        {msgs.map((msg, index) => (
-          <li key={index}>{msg}</li>
-        ))}
-      </ul>
+      {/* {<LoadMsg msg={user} />} */}
+      <h1>Hey {user}!</h1>
+      {/* <button onClick={show}>check</button><br /> */}
+
+      {msgs.length > 0 ? (
+        <ul>
+          {msgs.map((msg, index) => (
+            <li key={index}>{msg}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
+
     </div>
   )
 }
